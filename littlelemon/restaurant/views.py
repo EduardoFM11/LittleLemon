a2c9d766fb3c2d4f9fadcf77ec1c06fe5f9d1558
EduardoFM <fonglalo1119@gmail.com>
2023-05-20 20:40:38 -0600

    token authentication for menu and booking views

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,15 +3,18 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class BookingView(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
